450530b6d5400f8c677707d07002aa23e728aefd
kaiolabre <5704@ait.nsw.edu.au>
2018-11-11 15:06:07 +1100

    Adding Files Via Upload

---
+++ Kirribili LodgeTests/Kirribili_LodgeTests.m
@@ -0,0 +1,39 @@
+//
+//  Kirribili_LodgeTests.m
+//  Kirribili LodgeTests
+//
+//  Created by Kaio Labre on 4/9/18.
+//  Copyright © 2018 Kaio Labre. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Kirribili_LodgeTests : XCTestCase
+
+@end
+
+@implementation Kirribili_LodgeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
